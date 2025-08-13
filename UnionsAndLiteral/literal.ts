5aaef8b62e1acca23e740d64f04979ea197872d6
Taja <taja526526@gmail.com>
2025-08-14 00:05:13 +0800

    feat: chekc type

--- UnionsAndLiteral/literal.ts
+++ UnionsAndLiteral/literal.ts
@@ -3,3 +3,8 @@ const person = 'hypatia' // 'hypatia'
 
 // person = 'ya' // Error: Cannot assign to 'person' because it is a constant.
 test = person // literal can assign to string type
+
+
+let nameMaybe = Math.random() > 0.5 ? 'taja' : undefined
+// nameMaybe.toLocaleLowerCase() // if strictNullChecks （strict）啟用這行會報錯（嚴格空值檢查）
+
