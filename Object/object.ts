271f26e90f8cebc5fc0cf91553270ee0faea158e
Taja <taja526526@gmail.com>
2025-08-15 00:59:22 +0800

    feat: object type

--- Object/object.ts
+++ Object/object.ts
@@ -3,3 +3,17 @@ type Poet = {
     born: number,
     name: string
 }
+
+// 任何滿足型別的數值 可以用作該型別
+type WithFirstName = {
+    firstName:string
+}
+type WithLastName = {
+    lastName: string
+}
+const hasBoth = {
+    firistName: 'huang',
+    lastName: 'taja'
+}
+let withFirstName: WithLastName = hasBoth
+let withLastName: WithLastName = hasBoth
